45617990dfaf664e356c4db871cbce3d1b0696b4
ksncmounica <mounica.kokkerlapati@gmail.com>
2021-05-23 17:05:04 +0300

    unlayer module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from 'react';
+import EmailEditor from 'react-email-editor';
+
 
 function App() {
+  const emailEditorRef = useRef(null);
+
+  const saveDesign = () => {
+    emailEditorRef.current.editor.saveDesign((design) => {
+      console.log('saveDesign', design);
+      alert('Design JSON has been logged in your developer console.');
+    });
+  };
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log('exportHtml', html);
+      alert('Output HTML has been logged in your developer console.');
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>React Email Editor</h1>
+      <EmailEditor ref={emailEditorRef} />
+      <button onClick={saveDesign}>Save Design</button>
+      <button onClick={exportHtml}>Export HTML</button>
+      </div>
     </div>
   );
 }
-
 export default App;
